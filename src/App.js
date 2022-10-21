251a72016a0286eebf1522a6e9416624f5a0a813
faizan-exe <faizanofficial120@gmail.com>
2022-10-21 21:31:40 +0500

    added cors-anywhere

--- src/App.js
+++ src/App.js
@@ -10,11 +10,33 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import {useEffect} from 'react';
 
+
+
 function App() {
 
   useEffect(() => {
     document.title = "GDSC Assignment"
   });
+
+  const config = {
+    
+     
+    headers: {
+      Authorization: "Bearer 916o5kG3sg9yOutmdyp3qM0oWK3HoFShG3OYo79ne1PDDvlWsgK-vv90r4NDmP37zJr4zPaGYHEANer916lHyczvtkUdYRlzicPKSUMLMW-fp1sg5zfuxFi3C8NSY3Yx",
+      
+       
+    },
+    
+  };
+
+  axios
+    .get("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/WavvLdfdP6g8aZTtbBQHTw", config)
+    .then((response) => {
+      console.log(response); //These are the results sent back from the API!
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 
   return (
@@ -43,8 +65,11 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+    
   );
 }
+
+
 
 export default App;
 
